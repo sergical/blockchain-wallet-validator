94f092a66eb3d5df7373f3cb0273d9a7fffe2a7b
Rastislav <rastislav@onion.email>
2024-12-27 20:58:08 +0100

    Updated config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
-import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
-import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-plugin-prettier/recommended";
 
 export default [
   {
@@ -12,17 +11,10 @@ export default [
         ecmaVersion: "latest",
         sourceType: "module",
       },
-      globals: {
-        ...globals.node,
-      },
-    },
-    plugins: {
-      "@typescript-eslint": tsPlugin,
-      prettier: prettierPlugin,
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
-      ...prettierPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
     },
